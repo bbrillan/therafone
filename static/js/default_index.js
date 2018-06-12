9b9afe8dafeb3c71e5b6646966590046fd85c8d3
bbrillan <bbrillan@ucsc.edu>
2018-06-11 18:14:27 -0700

    only Home page info

--- static/js/default_index.js
+++ static/js/default_index.js
@@ -81,7 +81,7 @@ var app = function() {
                 $.web2py.enableElement($("#button_save_edit"));
                 self.vue.is_being_edited = false;
             });
-    },
+    };
 
     //Delete Button
     self.button_delete = function(memo_id) {
@@ -114,143 +114,6 @@ var app = function() {
     };
 
 
-    //******* Any functions for the activity page ***** //
-
-    //helper function for bounce()
-    self.up = function () {
-        var num = $(".dot");
-        num.slideUp(1000);
-        console.log(num);
-    };
-
-    //helper function for bounce()
-    self.down = function () {
-        var num = $(".dot");
-        num.slideDown(1000);
-        console.log(num);
-
-  /*
-        for (i=0; i < 400; i--){
-            $(".dot").postion += 1;
-        }
-*/
-
-
-     //   if (self.vue.duration >0) {
-     //       self.vue.pos -= 1;
-     //   }
-     //   else {
-      //      self.vue.pos = position;
-      //  }
-    };
-
-    self.stop_bounce = function(duration) {
-        console.log('stop?');
-        self.vue.stop_ball = true;
-        setTimeout(self.bounce, duration);
-    };
-
-
-    self.bounce = function (bpm, duration) {    // duration must be in seconds
-        var bps = bpm / 60;                     // setting beats per minute to beats per second
-        var num_beats = bps * duration;         // number of beats in the song
-        console.log(num_beats);
-        self.vue.show_balls = true;
-        var btime = bps/2;                      //the time the ball will be traveling up or down.
-
-        self.vue.show = false;
-
-
-            for (var i = 0; i <= num_beats; i++) {   //for every beat
-                console.log('in for loop1');
-                var hit = bps * i;                    //the time stamp for every beat
-                console.log(hit);
-                for (var j = 0; j < bps; j++) { //what is going to happen for each beat; i.e. within each bounce
-                    console.log('in for loop2');
-                    if (j < btime) {
-                        $(".dot").slideDown(bps * 1000);
-                        console.log('going down');
-                    } else {
-                        $(".dot").slideUp(bps * 1000);
-                        console.log('going up');
-                    }
-                }
-                duration -=1;
-                console.log(duration);
-            } //end for num_beats
-
-    };
-
-
-    self.play = function() {
-            var animation = document.getElementsByClassName('animated')[0];
-        //    $(".animated").animate('bounce-in', '2s', 'infinite');
-
-          //  animateDuration(2);
-
-
-   //             animationDuration: 1,
-    //            animationIterationCount: 3
-     //       }, "slow");
-
-            console.log('here?');
-
-         //   animation.css('animation-duration', 3);
-            self.vue.more_buttons = true;
-            console.log($(".animated"));
-            animation.classList.add('animated');
-            animation.style.animationPlayState = "running";
-    };
-
-
-    self.start = function () {
-        self.vue.show=true;
-        self.vue.more_buttons=false;
-        console.log('ok');
-
-    //    self.vue.plays.addEventListener('click', self.play, true);
-
-        var play = document.getElementById('play');
-        var pause = document.getElementById('pause');
-        var restart = document.getElementById('restart');
-        var dot = document.getElementById('dot');
-
-
-        var animation = document.getElementsByClassName('animated')[0];
-        //'pressing play makes the ball bounce'//
-
-
-        play.addEventListener('click', function(a) {
-            a.preventDefault();
-            self.vue.more_buttons = true;
-            animation.classList.add('animated');
-            animation.style.animationPlayState = "running";
-        }, false);
-
-
-        pause.addEventListener('click', function(a) {
-            a.preventDefault();
-            self.vue.more_buttons = true;
-            animation.style.animationPlayState = "paused";
-        }, false);
-
-
-        restart.addEventListener('click', function(a) {
-            a.preventDefault();
-            self.vue.more_buttons = false;
-            dot.classList.remove('animated');
-            void dot.offsetWidth;
-            animation.style.animationPlayState = "paused";
-        }, false);
-
-
-    };
-
-
-
-
-
-    //**************** End game functions **************//
 
 
     // Complete as needed.
@@ -268,24 +131,6 @@ var app = function() {
             edit_content: null,
             edit_id: 0,
 
-            //for activity page //
-
-            play_button: false,
-
-            speed: 0.0,
-            duration: 0.0,
-            show_balls: false, // boolean to display all the trailing balls
-            stop_ball: false,
-            show: true,
-            more_buttons: true,
-
-            plays: document.getElementById('play'),
-            pause: document.getElementById('pause'),
-            restart: document.getElementById('restart'),
-            dot: document.getElementById('dot'),
-
-             animation: document.getElementsByClassName('animated')[0],
-            //end activity page //
         },
         methods: {
             get_memos_url: self.get_memos_url,
@@ -299,24 +144,10 @@ var app = function() {
             button_delete: self.button_delete,
             button_toggle_public: self.button_toggle_public,
 
-            //for activity page //
-            up: self.up,
-            down:self.down,     //not needed? they are just helped functions ...
-
-            bounce: self.bounce,
-            stop_bounce: self.stop_bounce,
-
-            start: self.start,
-
-            play: self.play,
-
-
-            //end activity page //
         },
 
     });
 
-    self.start();
     self.get_memos();
     $("#vue-div").show();
     return self;
@@ -327,88 +158,3 @@ var APP = null;
 // This will make everything accessible from the js console;
 // for instance, self.x above would be accessible as APP.x
 jQuery(function(){APP = app();});
-
-
-
-// $dot.add("click", function() {
-
-      //  })
-
-        //        self.vue.dot.classList.add('animated');
-
-
-
-      /*  $('.dot:eq(1)').on('click', function () {
-            self.vue.dot.addClass('dot');
-        */
-
-
-    /*
-    self.bounce = function() {
-        for (var i = 0; i < self.vue.angles.length; i++){
-            self.vue.angle=self.vue.angles[i];
-
-            self.vue.x = self.vue.cx + self.vue.radius * Math.cos(self.vue.angle*self.vue.deg2rad);
-            self.vue.y = self.vue.cy + self.vue.radius * Math.sin(self.vue.angle*self.vue.deg2rad);
-
-            $("#ball" + i).css({left:self.vue.x - self.vue.radius/2, top:self.vue.y - self.vue.radius/2});
-
-            self.vue.angles[i] = self.vue.angles[i] + 1;
-            if (self.vue.angles[i] > 360){
-                self.vue.angles[i] = 0;  //resetting the angle to 0 at complete circle
-            }
-
-
-        }
-    };
-
-
-    self.bounce = function() {
-        self.vue.is_showing = !self.vue.is_showing;
-        console.log(self.vue.is_showing);
-    };
-
-    const Child = {
-        template: '#childarea'
-
-    };
-
-
-    self.up_or_down = function (position) {
-        for (i = 0; i < $(".game_container").height/2; i++) {
-            if (self.vue.pos = $(".game_container").height/2) {
-                self.vue.pos -= 1;
-            } else if (self.vue.pos = 0){
-                self.vue.pos += 1;
-            } else {
-                self.vue.pos = i;
-            }
-
-        }
-
-
-
-            if (self.vue.pos = $(".game_container").height/2) {
-                self.vue.pos -= 1;
-            } else if (self.vue.pos = 0) {
-                self.vue.pos += 1;
-            }
-    };
-
-
-    self.bounce = function() {
-        if (self.vue.duration > 0) {
-            if (self.vue.pos > $(".dot").position) {
-                self.vue.pos = $(".dot").position;
-            }
-             else if (self.vue.pos < $(".game_container").height/2) {
-
-            }
-
-        }
-
-    };
-
-
-
-    */
